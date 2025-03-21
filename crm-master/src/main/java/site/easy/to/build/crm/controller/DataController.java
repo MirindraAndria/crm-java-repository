a6a00c8840a7562883e15656b3b7b9b5a1dd6ffb
MirindraAndria <mixandria19@gmail.com>
2025-03-21 13:57:06 +0300

    commit reset
    reset data on crm

---
+++ crm-master/src/main/java/site/easy/to/build/crm/controller/DataController.java
@@ -0,0 +1,20 @@
+package site.easy.to.build.crm.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import site.easy.to.build.crm.service.data.DataService;
+
+
+@Controller
+public class DataController {
+    @Autowired
+    private DataService dataService ;
+
+    @PostMapping("/action")
+    public String action() {
+        dataService.clearDatabase(); 
+        return "redirect:/";  
+    }  
+}
---
+++ crm-master/src/main/java/site/easy/to/build/crm/repository/DataRepository.java
@@ -0,0 +1,24 @@
+package site.easy.to.build.crm.repository;
+
+import java.util.List;
+import java.util.Set;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DataRepository {
+    private JdbcTemplate jdbcTemplate;
+    
+    public DataRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    public void clearDatabase(Set<String> tablesToExclude) {
+        List<String> tables = jdbcTemplate.queryForList("SHOW TABLES;", String.class);
+        for (String table : tables) {
+            if (!tablesToExclude.contains(table)) {
+                jdbcTemplate.execute("TRUNCATE TABLE " + table + ";"); 
+            }
+        }
+        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 1;");
+    }
+}
